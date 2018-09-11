2dd27db654e7d06500d00d16c938e1ffba662669
mati <jmsalinas@uc.cl>
2018-09-11 16:23:19 -0300

    cambio script menu

--- js/menuprueba.js
+++ js/menuprueba.js
@@ -1,7 +1,34 @@
 
   
-  var checker = true;
-  $("#hamburger").click(function () {
+//   var checker = true;
+//   $("#hamburger").click(function () {
+//     targetLeft = checker ? "158px" : "20px";
+//     targetRight = checker ? "130px" : "20px";
+//     targetMiddle = checker ? "148px" : "20px";
+//     $("#hamburger").animate({left: targetLeft},400);
+//     $("#menuicon2").animate({left: targetRight},400);
+//     $("#menuicon3").fadeToggle(400); 
+//     $("#menuicon3").animate({left: targetMiddle},{queue: false},400);      
+//     checker ? checker = false : checker = true;
+// });
+
+
+
+
+// $(document).ready(function(){
+//   $('#hamburger').click(function(){
+//     $('.menu-hide').toggleClass('show');
+//   });
+// });
+
+
+var checker = true;
+var boton1 = document.getElementById("menuicon");
+var boton2 = document.getElementById("hamburger");
+
+
+
+function desplazar(){
     targetLeft = checker ? "158px" : "20px";
     targetRight = checker ? "130px" : "20px";
     targetMiddle = checker ? "148px" : "20px";
@@ -9,14 +36,27 @@
     $("#menuicon2").animate({left: targetRight},400);
     $("#menuicon3").fadeToggle(400); 
     $("#menuicon3").animate({left: targetMiddle},{queue: false},400);      
-    checker ? checker = false : checker = true;
-});
-
-
-
-
-$(document).ready(function(){
-  $('#hamburger').click(function(){
     $('.menu-hide').toggleClass('show');
-  });
+    checker ? (checker = false) : (checker = true);
+}
+
+
+
+  $("#hamburger").click(function () {
+  $(document).ready(function(){
+  desplazar();
 });
+});
+
+
+
+    $(document).ready(function(){
+    window.addEventListener('click', function(e){
+      var menu= document.getElementsByClassName("menu-hide show");
+   if (e.target.className != "menu-hide show" && checker===false && e.target!= boton1 && e.target != boton2) {
+        desplazar(); 
+
+     }
+
+    })
+})
