1abc29c5eea63d7b5dafeb9ec2e300515d8cfb4f
mati <jmsalinas@uc.cl>
2018-10-03 11:17:54 -0300

    update

--- js/menuprueba.js
+++ js/menuprueba.js
@@ -1,26 +1,4 @@
 
-  
-//   var checker = true;
-//   $("#hamburger").click(function () {
-//     targetLeft = checker ? "158px" : "20px";
-//     targetRight = checker ? "130px" : "20px";
-//     targetMiddle = checker ? "148px" : "20px";
-//     $("#hamburger").animate({left: targetLeft},400);
-//     $("#menuicon2").animate({left: targetRight},400);
-//     $("#menuicon3").fadeToggle(400); 
-//     $("#menuicon3").animate({left: targetMiddle},{queue: false},400);      
-//     checker ? checker = false : checker = true;
-// });
-
-
-
-
-// $(document).ready(function(){
-//   $('#hamburger').click(function(){
-//     $('.menu-hide').toggleClass('show');
-//   });
-// });
-
 
 var checker = true;
 var boton1 = document.getElementById("menuicon");
