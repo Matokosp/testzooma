b5ea88f3f2ddf7cf9944aa223a7b7a28b49cb3b4
mati <jmsalinas@uc.cl>
2018-10-08 13:37:47 -0300

    update preload

--- js/preload.js
+++ js/preload.js
@@ -1,4 +1,16 @@
-window.addEventListener("load", function(){
-	var load_screen = document.getElementById("load_screen");
-	document.body.removeChild(load_screen);
-});
+// PRELOAD POR CARGA
+
+
+// window.addEventListener("load", function(){
+// 	var load_screen = document.getElementById("load_screen");
+// 	document.body.removeChild(load_screen);
+// });
+
+
+// PRELOAD POR TIEMPO
+
+
+setTimeout(function(){        
+    $('#load_screen').fadeOut();
+    $('#load_screen').delay(150).fadeOut('slow'); 
+}, 4000);
